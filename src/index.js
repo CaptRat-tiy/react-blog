58267d678fafbf1a58114b3ba9d62bf100130c44
captrat <jpatricklogan@gmail.comgit config user.name>
2017-04-20 10:43:36 -0600

    feat(TEMP) screenIndex lets var choose screen to display"

--- src/index.js
+++ src/index.js
@@ -3,15 +3,34 @@ import ReactDOM from 'react-dom';
 import Screen1 from './screens/screen1'
 import Screen2 from './screens/screen2'
 import Screen3 from './screens/screen3'
-
 import './styles/app.css';
 
 class App extends Component {
 
   render(){
+    var screenIndex = 2;
+
+    var ActiveScreen
+
+    if(screenIndex === 1) {
+      ActiveScreen = <Screen1 />
+    }
+
+    if(screenIndex === 2) {
+      ActiveScreen = <Screen2 />
+    }
+
+    if(screenIndex === 3) {
+      ActiveScreen = <Screen3 />
+    }
+
+
     return (
     <div className="app">
-      <div className="app-header"></div>
+      <div className="app-header">
+        <p>THIS IS THE HEADER</p>
+      </div>
+
       <div className="app-wrapper">
         <div className="app-nav">
           <div className="nav-item screen1">
@@ -24,8 +43,9 @@ class App extends Component {
             <p>Screen 3</p>
           </div>
         </div>
+
         <div className="main-content">
-          <Screen3/>
+          { ActiveScreen }
         </div>
       </div>
     </div>
