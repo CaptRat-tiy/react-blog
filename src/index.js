9d5ef6e2df8d8a1008e7342256c0a2d02dd9f0b4
Patrick Logan <JPatrickLogan@gmail.com>
2017-04-20 00:58:00 -0600

    feat(css) WIP: styles not working yet.

--- src/index.js
+++ src/index.js
@@ -10,6 +10,15 @@ class App extends Component {
       <div className="app-header"></div>
       <div className="app-wrapper">
         <div className="app-nav">
+          <div className="nav-item screen1">
+            <p>Screen 1</p>
+          </div>
+          <div className="nav-item screen2">
+            <p>Screen 2</p>
+          </div>
+          <div className="nav-item screen3">
+            <p>Screen 3</p>
+          </div>
         </div>
         <div className="main-content">
           {this.props.children}
