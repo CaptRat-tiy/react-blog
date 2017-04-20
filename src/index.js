ab5831f1302e38a8d9ff927d37353d968baacae7
Patrick Logan <JPatrickLogan@gmail.com>
2017-04-19 23:57:03 -0600

    Remove extraneous/default files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './styles/app.css';
+
+class App extends Component {
+
+  render(){
+    return (
+    <h1>Hello</h1>
+    )
+  }
+}
 
 ReactDOM.render(
   <App />,
